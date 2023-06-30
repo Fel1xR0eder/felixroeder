9439f90c3978d9cfa141cf21938d9d7cc0a56221
Felix Roeder <xfeli537@gmail.com>
2023-06-30 13:17:32 +0200

    added onscroll animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,27 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'felixroeder';
 
-  constructor(public router: Router) { }
-  
+  constructor(public router: Router) { 
+
+  }
+
+  ngOnInit() {
+    window.addEventListener("scroll", this.reveal); 
+  }
+
+
+  reveal() {
+    let reveals = document.querySelectorAll(".reveal");
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
 }
