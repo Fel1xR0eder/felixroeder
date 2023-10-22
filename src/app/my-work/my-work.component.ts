190a6108d1fce8fc310754f5f8a511fb9940cc52
Fel1xR0eder <xfeli537@gmail.com>
2023-10-22 18:33:49 +0200

    small improvements

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -10,7 +10,6 @@ export class MyWorkComponent {
 
   isSafari:any = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-
   constructor(public app: AppComponent) { }
 
   projects = [
@@ -26,11 +25,10 @@ export class MyWorkComponent {
                 weisen Sie Benutzer und Kategorien zu.`,
       link: 'https://github.com/Fel1xR0eder/Join-Kanban-Tool',
       test: 'http://join.felixroeder.de/index.html '
-
     }, {
       name: 'Sharkie',
       img: './assets/img/sharkie.png',
-      language: 'Javascript OOP | CSS |HTML',
+      language: 'Javascript | CSS |HTML',
       spec: `Jump, run and throw game based on object-oriented approach.
             Help Sharkie to fight against evil jellyfishes, pufferfishes and the big orca.`,
       spec_de: `Sprung-, Lauf- und Wurfspiel auf der Grundlage eines objektorientierten Ansatzes. 
@@ -38,12 +36,11 @@ export class MyWorkComponent {
       link: 'https://github.com/Fel1xR0eder/Sharkie-Game',
       test: 'http://sharkie.felixroeder.de/index.html'
     }, {
-      name: 'Slack clone - in production',
+      name: 'DaBubble',
       img: './assets/img/slack_clone.png',
       language: 'Angular | SCSS | Firebase',
-      spec: `Be excited to see my latest group project. A business messenger similar to Slack. It will be released soon`,
-      spec_de: `Sei gespannt auf mein neuestes Gruppenprojekt. Ein Business messenger ähnlich wie Slack.
-                 Es wird bald veröffentlicht `,
+      spec: `DaBubble is a collaborative communication platform that enables teams to chat in real-time, exchange files, and organize projects.`,
+      spec_de: `DaBubble ist eine kollaborative Kommunikationsplattform, die es Teams ermöglicht, in Echtzeit zu chatten, Dateien auszutauschen und Projekte zu organisieren.`,
       link: 'https://github.com/MarcelBerlin/Angular-DABubble',
       test: 'http://dabubble.felixroeder.de/index.html'
     }
